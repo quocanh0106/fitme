f20032aff454ce1c74e9b707a094f0ae9b4df98e
anhdq <quocanh1699@gmail.com>
2024-01-15 13:09:30 +0700

    fitme

--- assets/template-series.js
+++ assets/template-series.js
@@ -9,35 +9,69 @@ class TabsComponent extends HTMLElement {
 
   handleTabClick(clickedTab) {
     if (!clickedTab.classList.contains('active')) {
+      this.pauseAllIframes();
       const activeTab = this.querySelector('.tab-heading.active');
       const activeTabId = activeTab.dataset.id;
       const targetId = clickedTab.dataset.id;
-
       this.hideTab(activeTabId);
       this.showTab(targetId);
-
       activeTab.classList.remove('active');
       clickedTab.classList.add('active');
     }
   }
 
   hideTab(tabId) {
-    if (this.classList.contains('normal-tab')) {
-      this.querySelector(`.tab-detail[data-id='${tabId}']`).classList.add('active');
-    } else {
-      this.querySelector(`.tab-detail[data-id='${tabId}']`).classList.add('active');
-    }
+    this.querySelector(`.tab-detail[data-id='${tabId}']`).classList.remove('active');
+
   }
 
   showTab(tabId) {
-    if (this.classList.contains('normal-tab')) {
-      this.querySelector(`.tab-detail[data-id='${tabId}']`).classList.remove('hidden');
-    } else {
-      this.querySelector(`.tab-detail[data-id='${tabId}']`).classList.remove('lg:hidden');
-    }
+    this.querySelector(`.tab-detail[data-id='${tabId}']`).classList.add('active');
+  }
+
+  pauseAllIframes() {
+    this.iframes = this.querySelectorAll('iframe');
+    this.iframes.forEach((iframe, key) => {
+      iframe.outerHTML = this.iframes[key].outerHTML;
+    });
   }
 }
 
 if (!customElements.get('tabs-component')) {
   customElements.define('tabs-component', TabsComponent);
+}
+
+if (!customElements.get('select-toggle')) {
+  class SelectToggle extends HTMLElement {
+    constructor() {
+      super();
+      this.summary = this.querySelector('.summary');
+      this.details = this.querySelector('.details');
+      this.summary.addEventListener('click', this.toggle.bind(this));
+    }
+
+    toggle() {
+      if (this.classList.contains('active')) {
+        this.details.style.maxHeight = 0;
+      } else {
+        this.details.style.maxHeight = this.details.scrollHeight + 'px';
+
+        if (this.classList.contains('clickout-accordion')) {
+          document.addEventListener('click', (e) => {
+            if (!this.contains(e.target)) {
+              this.details.style.maxHeight = 0;
+              this.classList.remove('active');
+            }
+          }, false);
+        }
+
+        
+      }
+      this.classList.toggle('active');
+    }
+
+
+  }
+
+  customElements.define('select-toggle', SelectToggle);
 }
